b2b1f0ffe4a1f753113490ac4133e35cbc952bf6
Buneabt <buneabt@yahoo.com>
2025-03-02 21:18:55 -0500

    Changed t.test parameters

--- Project2.R
+++ Project2.R
@@ -76,10 +76,10 @@ part3 <- function() {
 # ------------------------ Part 4 ------------------------
 
 part4 <- function() {
-  print(t.test(mass ~ type, data = df_transform))
+  print(t.test(mass ~ type, data = df_transform, var.equal = FALSE))
   
   
-  print(t.test(svl ~ type, data = df_transform))
+  print(t.test(svl ~ type, data = df_transform, var.equal = FALSE))
 }
 
 
